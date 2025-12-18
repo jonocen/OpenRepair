34345e256b81f2cb57ab0de8dacc6997d0bc6bd4
Jonocen <84701237+jonocen@users.noreply.github.com>
2025-12-18 20:14:25 +0100

    Add initial kernel repair utility
    Initial implementation of kernel repair utility with a placeholder for architecture enumeration and a Debian kernel repair function.

---
+++ libs/kernelrepair.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/*
+If you read this, im still working on it, i thout maybe i first find the arch of the traget system, 
+then find the package manager, then repair the kernel via reinstalling the kernel package.
+please be patient :)
+
+*/
+
+
+int main(){
+    printf("%d", enumarate_arch("/sys"));
+    return 0;
+}
+
+int debian_kernel_repair(){
+    
+    return 0;
+}
